18c1c3f35d232241618011e10171fa79493f9ae9
andrewcantrell <andyroocantrell@gmail.com>
2021-11-16 16:04:36 -0800

    Here's the initialized repo

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-import PySimpleGUI
+import PySimpleGUI as sg
+import PyPDF2 as pr
 
 
-def main():
-    PySimpleGUI.Popup()
-    print()
+sg.theme('Light Blue')
 
+layout = [  [sg.Text('Check email PDF for Phishing Content ><>', font=('Helvetica 15 bold'))],
+            [sg.Input(size=(50,50)), sg.FileBrowse()],
+            [sg.OK(), sg.Cancel()]]
 
-if __name__ == '__main__':
-    main()
+window = sg.Window('Phis In The Sea', layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel':
+        break
+    elif event == 'OK':
+        file = open(values[0], 'rb')
+        emailPDF = pr.PdfFileReader(file)
+        print(emailPDF.getPage(0).extractText())
+
+        file.close()
+
+window.close()
