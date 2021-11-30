7d617b30073a53c6683df9f54357974c7d8bbcfb
laurenmnelson <46546796+laurenmnelson@users.noreply.github.com>
2021-11-30 14:06:54 -0800

    adding links.py

--- links.py
+++ links.py
@@ -2,9 +2,9 @@ import webbrowser
 import PySimpleGUI as sg
 
 urls = {
-    'Gmail':'https://support.google.com/mail/answer/8151?hl=en&co=GENIE.Platform%3DDesktop',
-    'Outlook':'https://support.microsoft.com/en-us/office/block-senders-or-mark-email-as-junk-in-outlook-com-a3ece97b-82f8-4a5e-9ac3-e92fa6427ae4',
-    'Yahoo':'https://help.yahoo.com/kb/SLN28140.html?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAHtQ63y5LGI5OL8ypNe9vuT9a7NJ9rBpBlm45CZTm1zFkr03L_S8tVVT8o8Jzb_jhrzr6EAvGyut7OUKewo7FL1w47iEfF21ZjO1PMGdnlzyD4QorOXEYZXPZ_vTPqlsa1_XxP0jZNhS9mVlgPk8Yai3r7BkRZLUiWmtDkDLlkws',
+    'Gmail': 'https://support.google.com/mail/answer/8151?hl=en&co=GENIE.Platform%3DDesktop',
+    'Outlook': 'https://support.microsoft.com/en-us/office/block-senders-or-mark-email-as-junk-in-outlook-com-a3ece97b-82f8-4a5e-9ac3-e92fa6427ae4',
+    'Yahoo': 'https://help.yahoo.com/kb/SLN28140.html?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAHtQ63y5LGI5OL8ypNe9vuT9a7NJ9rBpBlm45CZTm1zFkr03L_S8tVVT8o8Jzb_jhrzr6EAvGyut7OUKewo7FL1w47iEfF21ZjO1PMGdnlzyD4QorOXEYZXPZ_vTPqlsa1_XxP0jZNhS9mVlgPk8Yai3r7BkRZLUiWmtDkDLlkws',
 }
 
 items = sorted(urls.keys())
