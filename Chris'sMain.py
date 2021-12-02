3b04b5d159feedba14b2a2f976988fa63bfdedde
cstyerwalt <christopherr.styerwalt@calbaptist.edu>
2021-12-02 10:50:20 -0800

    new push

--- Chris'sMain.py
+++ Chris'sMain.py
@@ -2,34 +2,51 @@ import PySimpleGUI as sg
 import PyPDF2 as pr
 
 
+import PySimpleGUI as sg
+import pikepdf as pr
+
+
 def main():
     sg.theme('Light Blue')
 
-    layout = [[sg.Text('Check email PDF for Phishing Content ><>', font='Helvetica 15 bold')],
+    layout = [[sg.Text('Check email PDF for Phishing Content ><>', font=('Helvetica 15 bold'))],
               [sg.Input(size=(50, 50)), sg.FileBrowse(file_types=(("PDF files", "*.pdf"),))],
-              [sg.OK(), sg.Cancel()]]
+              [sg.OK(button_text='Upload'), sg.Cancel(button_text='Forget it')]]
 
     window = sg.Window('Phish In The Sea', layout)
 
     while True:
         event, values = window.read()
-        if event == sg.WIN_CLOSED or event == 'Cancel':
+        if event == sg.WIN_CLOSED or event == 'Forget it':
             break
-        if event == 'OK':
-            try:
+        elif event == 'Upload':
+            try:  # TODO in order to prevent it from erroring if not links
+
+                # Scan the document and find phishing
                 file = open(values[0], 'rb')
-                emailPDF = pr.PdfFileReader(file)
-                print(emailPDF.getPage(0).extractText())
+                emailPDF = pr.Pdf.open(file)
+
+                links = []
+
+                for page in emailPDF.pages:
+                    for annots in page.get("/Annots"):
+                        uri = annots.get("/A").get("/URI")
+                        if uri is not None:
+                            links.append(uri)
+
+                for a in links:
+                    print(a)
                 file.close()
-                open_phishy_link_window()
-                break
-            except FileNotFoundError:
-                file_not_found_window()
+
+                phishy_link_window()
+            except:
+                no_phish_window()
+
 
     window.close()
 
 
-def open_phishy_link_window():
+def phishy_link_window():
     phishy_window = [[sg.Text(
         'This email contains a malicious link! Consider blocking the sender and \nnotifying people you know about the '
         'potential threat.',
@@ -72,7 +89,7 @@ def file_not_found_window():
 
 
 def block_sender_window():
-    print("blocked")
+    print("blocked -- NOT FINISHED")
 
 def notify_others_window():
     notify_others = [[sg.Text('Notify others', font='Helvetica 15 bold')],
@@ -89,6 +106,22 @@ def notify_others_window():
             break
         window.close()
         window = None
+
+def no_phish_window():
+    notify_others = [[sg.Text('No phishy links were found in your email!', font='Helvetica 15 bold')],
+                     [],
+                     [sg.OK()]]
+
+    window = sg.Window('Phish Free!', notify_others)
+    while True:
+        event, values = window.read()
+        if event == "OK" or event == sg.WIN_CLOSED:
+            window.close()
+            window = None
+            break
+        window.close()
+        window = None
+
 
 if __name__ == "__main__":
     main()
